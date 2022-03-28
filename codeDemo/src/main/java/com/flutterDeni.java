f7e1cf9e33835ff1f770db6e6d60050540b0a775
wangshuo <wangshuo@laiyuezs.com>
2022-03-28 11:37:06 +0800

    feat: 字符串正则替换

---
+++ codeDemo/src/main/java/com/flutterDeni.java
@@ -0,0 +1,18 @@
+package com;
+
+/**
+ * @Description
+ * @Author Ws
+ * @Date 2022-03-28 11:26
+ * @Version V1.0
+ */
+public class flutterDeni {
+
+
+    public static void main(String[] args) {
+
+
+
+        System.out.println("${initialIndex.xxx123}".replaceAll("(\\$\\{initialIndex\\.)(.*)(\\})","$1AAAA6666$3"));
+    }
+}
