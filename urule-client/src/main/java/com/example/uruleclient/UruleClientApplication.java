907d624685d1624572feb82fdf4ec99eead5116c
wangshuo <wangshuo@laiyuezs.com>
2022-06-21 17:40:47 +0800

    add urule

---
+++ urule-client/src/main/java/com/example/uruleclient/UruleClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.uruleclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UruleClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UruleClientApplication.class, args);
+    }
+
+}
