4aaeffc4e1061728e673c4f3cf145e47c3411d6a
eshuo <31001209+eshuo@users.noreply.github.com>
2022-06-23 17:50:36 +0800

    Delete p2p-core directory

--- p2p-core/src/main/java/info/bfly/archer/Constants.java
+++
@@ -1,5 +0,0 @@
-package info.bfly.archer;
-
-public class Constants {
-    public final static String APP_BASE_PATH = Constants.class.getPackage().toString();
-}
--- p2p-core/src/main/java/info/bfly/archer/banner/service/BannerService.java
+++
@@ -1,7 +0,0 @@
-package info.bfly.archer.banner.service;
-
-import info.bfly.archer.banner.model.BannerPicture;
-
-public interface BannerService {
-    void deleteBannerPicture(BannerPicture bannerPicture);
-}
