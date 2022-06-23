85d779ab7bc22e3c82723617ab4e5d4c2e30b379
eshuo <31001209+eshuo@users.noreply.github.com>
2022-06-23 17:51:53 +0800

    Delete malldemo directory

--- malldemo/p2p-core/src/main/java/info/bfly/archer/node/package-info.java
+++
@@ -1,2 +0,0 @@
-package info.bfly.archer.node;
-
--- malldemo/p2p-core/src/main/java/info/bfly/archer/node/service/NodeService.java
+++
@@ -1,7 +0,0 @@
-package info.bfly.archer.node.service;
-
-import info.bfly.archer.node.model.Node;
-
-public interface NodeService {
-    void save(Node node);
-}
